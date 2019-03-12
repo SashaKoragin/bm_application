766961d227c823ba686d1769fac1a1b8baa7bf7e
Илгиз Тимруков <tim.ilgiz@gmail.com>
2019-03-13 01:04:47 +0300

    мм

--- bm_application/MainWindow.xaml.cs
+++ bm_application/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace bm_application
 {
@@ -20,6 +22,9 @@ namespace bm_application
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Button button = new Button();        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,10 +32,68 @@ namespace bm_application
 
         private void button_click_home(object sender, EventArgs e)
         {
+            canvas.Children.Clear();
             ImageBrush myBrush = new ImageBrush();
             myBrush.ImageSource = new BitmapImage(new Uri(@"Resources\bm_group.png", UriKind.Relative));
             canvas.Background = myBrush;
             myBrush.Stretch = Stretch.Fill;
         }
+
+        private void button_click_url_home(object sender, EventArgs e)
+        {
+            canvas.Background = new SolidColorBrush(Colors.White);
+            WebBrowser browser = new WebBrowser();
+            browser.Navigate("http://www.bm-technology.ru");
+            canvas.Children.Add(browser);
+            browser.Width = 630;
+            browser.Height = 400;
+            //canvas.Children.Add(button);
+            //button.Click += MyBack_Click;
+            //button.Click += MyForward_Click;
+        }
+
+        private void Button_video_Click(object sender, RoutedEventArgs e)
+        {
+            var mediaPlayer = new MediaElement();
+
+            mediaPlayer.HorizontalAlignment = HorizontalAlignment.Left;
+            mediaPlayer.VerticalAlignment = VerticalAlignment.Top;
+            mediaPlayer.Height = 630;
+            mediaPlayer.Width = 400;
+            mediaPlayer.Source = new Uri("https://www.youtube.com/watch?v=UZSFY8YdSrA", UriKind.Absolute);
+
+            mediaPlayer.LoadedBehavior = MediaState.Manual;
+            if (mediaPlayer != null)
+            {
+                canvas.Children.Add(mediaPlayer);
+                mediaPlayer.Play();
+            }
+        }
+
+        //private void MyBack_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        browser.GoBack();              
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        //private void MyForward_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        browser.GoForward();
+        //        button.Height = 50;
+        //        button.Width = 50;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
     }
 }
